73ec5d218b6710730c7e46ce6ba9287d9ff7beae
Yue Sun <ysun276@wisc.edu>
2019-11-21 21:23:13 -0500

    Updates

---
+++ precache-manifest.4793cccebd0a84287ab07766e0af78af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a67c7aaac794bacc66a9393bbb452703",
+    "url": "/react-robotpage/index.html"
+  },
+  {
+    "revision": "53ef5564d911eed9bbb6",
+    "url": "/react-robotpage/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "53de111d6cc8d44d2292",
+    "url": "/react-robotpage/static/css/main.afce3dcc.chunk.css"
+  },
+  {
+    "revision": "53ef5564d911eed9bbb6",
+    "url": "/react-robotpage/static/js/2.7181e3f3.chunk.js"
+  },
+  {
+    "revision": "53de111d6cc8d44d2292",
+    "url": "/react-robotpage/static/js/main.1f8997af.chunk.js"
+  },
+  {
+    "revision": "9e3a0d326e83d67089aa",
+    "url": "/react-robotpage/static/js/runtime-main.6eaea879.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robotpage/static/media/SEGA.12a3db8b.woff"
+  }
+]);
